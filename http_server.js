15a8b44bed9939bf153df4f933d0eacb27f96066
Jim Amato <Amato1891@yahoo.com>
2021-12-16 15:16:14 -0500

    changed ports

--- http_server.js
+++ http_server.js
@@ -5,7 +5,7 @@ app.get('/',function(req,res){
 res.send('hello world!');
 });
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 5000,function(){
 console.log('running on port 3000!');
 });
 
